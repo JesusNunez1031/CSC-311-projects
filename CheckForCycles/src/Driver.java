e0a5e8304b6d0c478ff4b7c30c4e81c6d1e6e3cd
Jesus Nunez <39150487+JesusNunez1031@users.noreply.github.com>
2018-12-10 15:47:21 -0800

    Add files via upload

---
+++ CheckForCycles/src/Driver.java
@@ -0,0 +1,28 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// Create a graph given in the above diagram
+		Graph g1 = new Graph(5);
+		g1.addEdge(1, 0);
+		g1.addEdge(0, 2);
+		g1.addEdge(2, 0);
+		g1.addEdge(0, 3);
+		g1.addEdge(3, 4);
+		if (g1.isCyclic()) {
+			System.out.println("Graph contains cycle");
+		} else {
+			System.out.println("Graph does not contain cycle");
+		}
+		// Create another graph
+		Graph g2 = new Graph(3);
+		g2.addEdge(0, 1);
+		g2.addEdge(1, 2);
+		if (g1.isCyclic()) {
+			System.out.println("Graph contains cycle");
+		} else {
+			System.out.println("Graph does not contain cycle");
+		}
+	}
+
+}
